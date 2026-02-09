996708a69bbdad79a686cc6d279256db1f7a2f36
Cumbarcum <assasaasigma@gmail.com>
2026-02-09 16:59:31 +0300

    Delete assets/Stages/output_tileset.tsx

--- assets/Stages/output_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="output_tileset" tilewidth="16" tileheight="16" spacing="2" margin="2" tilecount="21" columns="3">
- <image source="../../Downloads/TilesCollisionMask/collmap/tilesets/tCollision/output_tileset.png" width="68" height="136"/>
-</tileset>
