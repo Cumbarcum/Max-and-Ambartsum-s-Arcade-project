92c4739bbcf750d093ed6b780d335cbcce2ad547
Cumbarcum <assasaasigma@gmail.com>
2026-02-09 17:01:01 +0300

    Add files via upload

---
+++ assets/Земля и блоки.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="NES - Super Mario Bros. - Miscellaneous - Tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="24" columns="8">
+ <image source="../../../Downloads/NES - Super Mario Bros. - Miscellaneous - Tileset.png" width="135" height="50"/>
+</tileset>
